6d1977170c32b5ffccc1cde25d9687a5ab7e6018
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-05-04 11:52:08 +0200

    loop in main.py

--- code/Reimplementation/main.py
+++ code/Reimplementation/main.py
@@ -4,13 +4,16 @@ import json
 import os
 
 def main():
+
+    # load configs from json file
+    configs = []
     with open(os.path.join(os.path.dirname(__file__), "Configs", "configs.json"), "r") as json_file:
-        data = json.load(json_file)
+        configs = json.load(json_file)
 
-        print(data[0])
-
-        controller = Controller(os.path.join(os.path.dirname(__file__), "results.csv"))
-        controller.set_config(data[1])
+    # for each config start the controller
+    controller = Controller(os.path.join(os.path.dirname(__file__), "results.csv"))
+    for config in configs:
+        controller.set_config(config)
         controller.start()
 
 if __name__ == "__main__":
