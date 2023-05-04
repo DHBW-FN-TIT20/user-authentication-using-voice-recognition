23ff73a8bfb7bc6d41bda730f06ea3d788f628c0
Lukas Braun <lb.0108@web.de>
2023-05-04 10:29:37 +0200

    Replaced setter and added in Ctor, added all Getter

--- code/Reimplementation/Evaluator.py
+++ code/Reimplementation/Evaluator.py
@@ -1,25 +1,24 @@
 import TestResult
 class Evaluator:
-    def __init__(self, neural_network, neural_network_id):
+    def __init__(self, neural_network, neural_network_id, test_data):
         self.results = None
         self.neural_network = neural_network
         self.neural_network_id = neural_network_id
-        self.test_data = None
+        self.test_data = test_data
 
     """Evaluates the neural network with the test data and saves the results"""
     def evaluate(self):
         pass
-    
-    def set_test_data(self, X, y):
-        # TODO: Überlegen wegen test-data array
-        pass
-
-    def set_neural_network(self, neural_network, neural_network_id):
-        self.neural_network = neural_network
-        self.neural_network_id = neural_network_id
 
     def get_test_data(self):
         return self.test_data
     
     def get_results(self):
         return self.results
+
+    def get_neural_network_id(self):
+        return self.neural_network_id
+    
+    def get_neural_network(self):
+        return self.neural_network
+    
