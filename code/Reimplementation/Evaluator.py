46f77d3fd8b193bf17f1d94b721eaff92184b35f
Lukas Braun <lb.0108@web.de>
2023-05-04 19:42:06 +0200

    requested changes

--- code/Reimplementation/Evaluator.py
+++ code/Reimplementation/Evaluator.py
@@ -1,18 +1,18 @@
 import TestResult
 import numpy as np
 class Evaluator:
-    def __init__(self, neural_network, neural_network_id, test_dataX, test_dataY):
+    def __init__(self, neural_network, neural_network_id, test_data_x, test_data_y):
         self.results = None
         self.neural_network = neural_network
         self.neural_network_id = neural_network_id
-        self.test_data_X:list[list[int]] = test_dataX # [[[]]] for each file, for each frame, X input values
-        self.test_data_Y:list[int] = test_dataY # [[]] for each file, for each Frame the same speaker_id
+        self.test_data_x = test_data_x # [[[]]] for each file, for each frame, X input values
+        self.test_data_y = test_data_y # [[]] for each file, for each Frame the same speaker_id
 
     """Evaluates the neural network with a prediction for test data and saves the results"""
     def evaluate(self):
-        for i in range(len(self.test_data_X)): # for each file
-            prediction = self.neural_network.predict(self.test_data_X[i])  # generate prediction for the sample
-            speaker_id = self.test_data_Y[i][0] # get the speaker_id of the sample
+        for i in range(len(self.test_data_x)): # for each file
+            prediction = self.neural_network.predict(self.test_data_x[i])  # generate prediction for the sample
+            speaker_id = self.test_data_y[i][0] # get the speaker_id of the sample
             sample_id = i%5 # 0-4
             # TODO Calculate Correspondence in % and add it to the results
             result = TestResult(speaker_id = speaker_id, sample_id = sample_id, correspondence = None)  # create a TestResult object
@@ -20,7 +20,7 @@ class Evaluator:
         pass
 
     def get_test_data(self):
-        return self.test_data
+        return self.test_data, self.test_data_y
     
     def get_results(self):
         return self.results
