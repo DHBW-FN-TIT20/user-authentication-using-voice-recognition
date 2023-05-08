127c135da493fea135a7a87bfa3e3f0847d4cca3
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-05-08 08:05:05 +0200

    self.calculate

--- code/Reimplementation/FeatureExtractor/MFCCExtractor.py
+++ code/Reimplementation/FeatureExtractor/MFCCExtractor.py
@@ -3,18 +3,21 @@ from multiprocessing import Pool
 import numpy as np
 import librosa
 
-def calculate_mfcc(frame, sr, order):
-    mfcc = librosa.feature.mfcc(y=frame, sr=sr, n_mfcc=order, n_fft=1024, hop_length=512)
-    mfcc_of_frame = np.mean(mfcc.T, axis=0)
-    return mfcc_of_frame
 
 class MFCCExtractor(ExtractorInterface):
+
+    @staticmethod
+    def calculate_mfcc(frame, sr, order):
+        mfcc = librosa.feature.mfcc(y=frame, sr=sr, n_mfcc=order, n_fft=1024, hop_length=512)
+        mfcc_of_frame = np.mean(mfcc.T, axis=0)
+        return mfcc_of_frame
+
     def calculate_features(self, frames, sr, order, multiprocessing=False):
 
         if multiprocessing:
 
             with Pool(8) as p:
-                mfccs = p.starmap(calculate_mfcc, [(frame, sr, order) for frame in frames])
+                mfccs = p.starmap(self.calculate_mfcc, [(frame, sr, order) for frame in frames])
 
             return mfccs
 
@@ -22,6 +25,6 @@ class MFCCExtractor(ExtractorInterface):
 
             mfccs = []
             for frame in frames:
-                mfccs.append(calculate_mfcc(frame, sr, order))
+                mfccs.append(self.calculate_mfcc(frame, sr, order))
 
             return mfccs
